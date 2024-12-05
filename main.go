a1ad834fdd6adb18af4ee7458ff37d73f9295b9d
BoyChai <1972567225@qq.com>
2024-12-05 15:03:13 +0800

    feat: update default export directory

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"GitHacker/recovery"
 	"flag"
 	"fmt"
+	"strings"
 )
 
 func main() {
 	t := flag.String("t", "url", "指定类型,默认为url")
-	recovery.OutputDir = *flag.String("o", "GitHacker_Output", "输出目录,默认值为当前位置的GitHacker_Output目录")
-
+	out := flag.String("o", "", "输出目录,默认值为当前位置的GitHacker_Output的主机地址目录下")
 	flag.Parse()
 
 	args := flag.Args()
@@ -17,6 +17,14 @@ func main() {
 		fmt.Println("传入参数有误，请查看使用说明")
 		return
 	}
+
+	if *out == "" {
+		withoutProtocol := strings.Split(args[0], "://")[1]
+		recovery.OutputDir = "GitHacker_Output/" + strings.Split(withoutProtocol, "/")[0]
+	} else {
+		recovery.OutputDir = *out
+	}
+
 	switch *t {
 	case "url":
 		recovery.UrlRecovery(args[0])
